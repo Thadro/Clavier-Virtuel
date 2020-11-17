31a97df84c9bc9e63ad8539abfb07868d53485e9
Thadro <romain.deneuve@yahoo.fr>
2020-11-17 14:50:34 +0100

    Javascript Added

--- keyboard.js
+++ keyboard.js
@@ -1,1 +1,26 @@
-console.log("ok")
+window.addEventListener("load", function ()
+{
+    const chat = document.querySelector("#chat");
+    const buttons = document.querySelectorAll("input[type=button]");
+
+
+
+    buttons.forEach(button =>
+        button.addEventListener("click", function ()
+        {
+            switch(this.value)
+            {
+                case "SPACE":
+                    chat.value += " ";
+                break;
+                case "DEL":
+                    chat.value = chat.value.substring(0, chat.value.lenght - 1);
+                break;
+                case "ENTER":
+                    chat.value += "\n";
+                break;
+                default:
+                    chat.value += this.value;
+                }                
+        }))
+})
